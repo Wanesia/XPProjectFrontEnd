a4c3e8bb19264e52baad9bacba94be5f8e1b7cc9
Daniel <dannysht27@gmail.com>
2022-09-28 09:31:52 +0200

    added save button functionality to paint tables

--- source/js/dining.js
+++ source/js/dining.js
@@ -1,9 +1,23 @@
 const tables = document.querySelectorAll(".table")
 const modal = document.getElementById("modal")
+const modalTitle = document.querySelector(".modal-title")
+const closeButton = document.getElementById("closeButton")
+const saveButton = document.getElementById("saveButton")
+
 tables.forEach(addClicker)
-function addClicker(table)
-{
-    table.addEventListener("click", e => {modal.style.display="block"})
+tables.forEach(changeBackground)
+
+
+
+function addClicker(table) {
+    table.addEventListener("click", function() {
+        modal.style.display="block";
+        modalTitle.innerHTML = table.id;
+        saveButton.addEventListener("click", e => {table.style.backgroundColor="red";modal.style.display="none"})
+        closeButton.addEventListener("click", e=> {modal.style.display="none"})
+        if(table.id !== modalTitle.innerHTML)
+        {
+            table.style.backgroundColor="purple"
+        }
+    })
 }
-
-
