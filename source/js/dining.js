c76117f31eab08d815762702210dfbfb8303f0f1
Daniel <dannysht27@gmail.com>
2022-09-28 10:32:27 +0200

    added some frontend functionality to the tables

--- source/js/dining.js
+++ source/js/dining.js
@@ -3,21 +3,45 @@ const modal = document.getElementById("modal")
 const modalTitle = document.querySelector(".modal-title")
 const closeButton = document.getElementById("closeButton")
 const saveButton = document.getElementById("saveButton")
+const inputs = document.querySelectorAll(".inputs")
+const dateTimeInput = document.querySelector(".date-picker")
+const timeInput = document.querySelector(".time-picker")
+const startDate = document.getElementById("startDate")
+const time = document.getElementById("time")
 
-tables.forEach(addClicker)
-tables.forEach(changeBackground)
+function dateOnChange()
+{
+    console.log(timeInput.value)
+    if(timeInput.value !== "")
+    {
+        tables.forEach(addClicker)
+    }
+}
 
+function timeOnChange()
+{
+    console.log(dateTimeInput.value)
 
+    if(dateTimeInput.value !== "")
+    {
+        tables.forEach(addClicker)
+    }
+}
 
 function addClicker(table) {
     table.addEventListener("click", function() {
+        startDate.innerHTML = "Date: " + dateTimeInput.value
+        time.innerHTML = "Time: " + timeInput.value
         modal.style.display="block";
         modalTitle.innerHTML = table.id;
-        saveButton.addEventListener("click", e => {table.style.backgroundColor="red";modal.style.display="none"})
-        closeButton.addEventListener("click", e=> {modal.style.display="none"})
-        if(table.id !== modalTitle.innerHTML)
-        {
-            table.style.backgroundColor="purple"
-        }
+        saveButton.addEventListener("click", e => {modal.style.display="none";
+            if(table.id == modalTitle.innerHTML && table.style.backgroundColor !== "red")
+            {
+                table.style.backgroundColor="red";
+            }
+            inputs.forEach(input=>input.value="")
+
+        })
+        closeButton.addEventListener("click", e=> {modal.style.display="none";inputs.forEach(input=>input.value="")})
     })
 }
