86e91e31d488d7f9b7e01609546286008af46cf3
Daniel <dannysht27@gmail.com>
2022-09-28 09:00:49 +0200

    changed to flex display for tables and made tables display modal when clicked

--- source/js/dining.js
+++ source/js/dining.js
@@ -1,3 +1,9 @@
-
+const tables = document.querySelectorAll(".table")
+const modal = document.getElementById("modal")
+tables.forEach(addClicker)
+function addClicker(table)
+{
+    table.addEventListener("click", e => {modal.style.display="block"})
+}
 
 
