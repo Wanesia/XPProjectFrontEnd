b4426334245d44500d08a8beb96ac66c934d7445
zbahramzy <zalandbahramzy@gmail.com>
2022-09-30 13:03:03 +0200

    fixed the stock manage page.

--- source/js/stock_manage.js
+++ source/js/stock_manage.js
@@ -1,40 +1,36 @@
-
 // REST API URLs to connect to back-end
-//const localStockApi = "http://localhost:8080/api/v1/stock";
+const localStockApi = "http://localhost:8080/api/v1/stock";
 
 // Table itself retrieved from the HTML document
-const manageStockTable = document.getElementById('stockManageTable')
+const stockTable = document.getElementById('stockManageTable')
 
-let stockArray = []
+let stockArray = [];
 
-function createRow(stock) {
-    // Row which is currently being generated established by the entity stock name from the back-end
-    const rowCount = stock.name;
-    const quantity = stock.quantity
-    // Generating row itself, no cells yet
-    let row1 = manageStockTable.insertCell(1)
-    let row1_2 = manageStockTable.insertCell(2)
+function createTable(stock) {
 
+    let cellCount = 0
+    let rowCount = stockTable.rows.length
+    console.log("rowcount = " + rowCount)
+
+    let row = stockTable.insertRow(rowCount)
+
+    let cell = row.insertCell(cellCount++)
+    cell.innerHTML = stock.name
+
+    cell = row.insertCell(cellCount++);
+    let inputField = document.createElement("input");
+    inputField.type = "number"
+    inputField.value = stock.quantity
+    cell.appendChild(inputField)
 }
 
-function doFetchStock() {
-    const name1 = stock.name
-    const quantity1 = stock.quantity
-    let table = manageStockTable;
-    let row1 = table.insertRow(1);
-    let cell1 = row1.insertCell(0)
-    let cell2 = row1.insertCell(1)
-    cell1.innerHTML = name1
-    cell2.innerHTML = quantity1
-}
-
-/*async function fetchStock(url) {
-    return fetch(url).then(response => reponse.json())
+async function fetchStock(url) {
+    return fetch(url).then(response => response.json())
 }
 
 async function doFetchStock() {
     stockArray = await fetchStock(localStockApi)
-    stockArray.forEach(createRow)
-}*/
+    stockArray.forEach(createTable)
+}
 
 document.addEventListener('keydown', doFetchStock)
