07c913974425a1f869faa50bf8d3019ee24eb73f
A-Stroem <asjnyborg@hotmail.com>
2022-10-04 09:17:23 +0200

    dropdown for stock order

--- source/js/stock_order.js
+++ source/js/stock_order.js
@@ -1,6 +1,37 @@
 const localStockOrderApi = "http://localhost:8080/api/v1/stock";
 
 const stockDropDown = document.getElementById('stock-drop-down')
+stockDropDown.length = 0;
+let defaultOption = document.createElement('Option')
+defaultOption.text = 'Select Stock'
+
+stockDropDown.add(defaultOption)
+stockDropDown.selectedIndex = 0;
+
+fetch(localStockOrderApi)
+    .then(
+        function(response) {
+            if (response.status !== 200) {
+                console.warn('We are f*cked but we dont have to pay bitcoin' + response.status);
+                return;
+            }
+
+            response.json().then(function (stock) {
+                let option;
+
+                for (let i = 0; i < stock.length; i++) {
+                    option = document.createElement('option')
+                    option.text = stock[i].id
+                    option.text = stock[i].name
+                    option.text = stock[i].quantity
+                    stockDropDown.add(option);
+                }
+            });
+        }
+    )
+    .catch(function(err) {
+        console.error('Fetch Error -', err)
+    });
 
 window.onload = loadIntoTable();
 
@@ -31,11 +62,13 @@ function loadIntoTable() {
 }
 
 
+
+
 let value = stockDropDown.value
 let text = stockDropDown.options[stockDropDown.selectedIndex].text
 
 function showStocktable() {
-    if  (document.getElementById("stock-drop-down").value === "Bowling" && json.value.name === "bowling") {
+    if  (document.getElementById("stock-drop-down").value === "Bowling") {
         loadIntoTable()
     }
 
