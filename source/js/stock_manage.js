2574df56a69dd70d0552593286fbf89b7989925d
zbahramzy <zalandbahramzy@gmail.com>
2022-10-04 09:01:26 +0200

    Added stof to the stock.css. Fixed the stock manage page to update stock quantity in the backend.

--- source/js/stock_manage.js
+++ source/js/stock_manage.js
@@ -7,21 +7,56 @@ const stockTable = document.getElementById('stockManageTable')
 let stockArray = [];
 
 function createTable(stock) {
-
     let cellCount = 0
     let rowCount = stockTable.rows.length
-    console.log("rowcount = " + rowCount)
 
     let row = stockTable.insertRow(rowCount)
 
     let cell = row.insertCell(cellCount++)
     cell.innerHTML = stock.name
 
-    cell = row.insertCell(cellCount++);
+    cell = row.insertCell(cellCount);
     let inputField = document.createElement("input");
     inputField.type = "number"
     inputField.value = stock.quantity
+    inputField.className = "stockTableElement"
     cell.appendChild(inputField)
+    let updateButton = document.createElement('button')
+    updateButton.innerText = "Update";
+    updateButton.className = "stockTableElement"
+    cell.appendChild(updateButton);
+    updateButton.addEventListener('click', () => {
+        updateStock(stock, inputField.value);
+    })
+}
+
+async function updateStock(stock, newQuantity) {
+    stock.quantity = newQuantity;
+    console.log(stock)
+    const response = await restUpdateStock(stock);
+    console.log(response)
+}
+
+async function restUpdateStock(stock) {
+    const url = localStockApi + "/" + stock.id;
+
+    const fetchOptions = {
+        method: 'PUT',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: ""
+    }
+
+    const jsonString = JSON.stringify(stock);
+    console.log("kjlksdjfld" + jsonString)
+    fetchOptions.body = jsonString;
+
+    console.log(url+fetchOptions)
+    const response = await fetch(url, fetchOptions)
+    console.log(response)
+
+    return jsonString;
 }
 
 async function fetchStock(url) {
@@ -32,5 +67,3 @@ async function doFetchStock() {
     stockArray = await fetchStock(localStockApi)
     stockArray.forEach(createTable)
 }
-
-//document.addEventListener('keydown', doFetchStock)
