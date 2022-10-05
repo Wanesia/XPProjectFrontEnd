14b7019b214a17e6b21aa8a52914c43e17a1e18e
Roman-Octavian <work.octavian@gmail.com>
2022-10-05 03:59:30 +0200

    Hockey: Moderate UI Redesign + Cleanup & Bug Fixes
    UI for hockey menu has been modified slightly to look less unappealing. Script has been cleaned a little bit. Modal has been added without functionality. Glitches such as table duplication have been fixed in hockey.

--- source/js/hockey-page.js
+++ source/js/hockey-page.js
@@ -1,9 +1,11 @@
+const modal = document.getElementById("modal");
+
 // REST API URLs to connect to back-end
 const localTableApi = "http://localhost:8080/api/v1/hockey-table";
 const localHockeyBookingApi = "http://localhost:8080/api/v1/hockey-booking";
 
 // Table from HTML doc
-const bookingTable = document.getElementById("bookingtable3")
+const tableBody = document.getElementById("tableBody");
 // HTML button
 const buttCreateTable = document.getElementById("generateTable")//button to create mockup
 // HTML date input
@@ -12,23 +14,52 @@ let datePicker = document.getElementById("date");
 let tableArr = [];
 let bookingArr = [];
 
-function createHockeyTableCountColumn(row, rowcount) {
-    // Generate the first cell, used for the row number
-    let tableNumber = row.insertCell();
-    // Establish first cell text as the table number
-    tableNumber.innerText = rowcount;
+/**
+ * Used as callback function on cell event listener.
+ * Will either show or hide a modal (pop-up window).
+ */
+function handleModal() {
+    // If the modal is being displayed, hide it and stop the rest of the function from executing
+    if (!(modal.style.display === "none")) {
+        modal.style.display = "none";
+        return;
+    }
+    // If the modal is not being displayed, display it
+    modal.style.display = "block";
+    // Get references to all elements in the modal from HTML using IDs
+    // Title
+    const modalTitle = document.getElementById("modalTitle");
+    // Input fields
+    const startDateTime = document.getElementById("startDateTime");
+    const endDateTime = document.getElementById("endDateTime");
+    const customerName = document.getElementById("customer");
+    const customerTelephone = document.getElementById("customerTelephone");
+    // Buttons
+    const bookButton = document.getElementById("bookButton");
+    const confirmChangesButton = document.getElementById("confirmChangesButton");
+    const cancelButton = document.getElementById("cancelButton");
+    const closeButton = document.getElementById("closeButton");
+
+    closeButton.addEventListener('click', handleModal);
 }
 
-// Adds button with eventListeners
-// Needs documentation, but it's 2 AM so not now
-function addButton(cell, bookingArr, rowCount) {
-    let bookingButton = document.createElement('button');
-    bookingButton.innerText = "Book now";
-    bookingButton.setAttribute('style','background-color: #157d31');
-    bookingButton.addEventListener('click',function bookButtonAction() {
-        bookingButton.innerText = "Booked";
-        bookingButton.setAttribute('style', 'background-color: #7d1515');
-    });
+function loadIndividualCell(cell, bookingArr, rowCount) {
+
+    cell.addEventListener('click', handleModal);
+    cell.classList.add("interactive-cell");
+
+    function formatCellText(isFree) {
+        if (!isFree) {
+            cell.innerText = "Booked";
+            cell.setAttribute("style","background-color: #f25459; text-align: center;");
+            return;
+        }
+        cell.innerText = "Free";
+        cell.setAttribute("style","background-color: #8be78b; text-align: center; color: black;");
+    }
+
+    formatCellText(true);
+
     // forEach won't work here because it does not support break
     for (let booking of bookingArr) {
         if (booking['hockeyTable']['id'] === rowCount) {
@@ -39,24 +70,11 @@ function addButton(cell, bookingArr, rowCount) {
             selectedStartDateTime.setHours(cell.timeSlot);
             selectedEndDateTime.setHours(cell.timeSlot + 1);
 
-            console.log(
-                "\nbs: " + bookingStartDateTime,
-                "\nbe: " + bookingEndDateTime,
-                "\nsd: " + selectedStartDateTime,
-                "\nsdp: " + selectedEndDateTime);
-
             if (bookingStartDateTime <= selectedStartDateTime && bookingEndDateTime >= selectedEndDateTime) {
-                bookingButton.innerText = "Booked";
-                bookingButton.setAttribute('style','background-color: #7d1515');
-                bookingButton.addEventListener('dblclick', function bookButtonAction(){
-                    bookingButton.innerText = "Book now";
-                    bookingButton.setAttribute('style','background-color: #157d31');
-                });
-                cell.appendChild(bookingButton);
+                formatCellText(false);
                 break;
             }
         }
-        cell.appendChild(bookingButton);
     }
 }
 
@@ -64,15 +82,18 @@ function createRow(table) {
     // Row which is currently being generated established by the entity ID from the back-end
     const rowCount = table.id;
     // Generating row itself, no cells yet.
-    let row = bookingTable.insertRow(rowCount)
-    createHockeyTableCountColumn(row, rowCount);
+    let row = tableBody.insertRow(rowCount - 1)
+    // Generate the first cell, used for the row number
+    let tableNumber = row.insertCell();
+    // Establish first cell text as the table number
+    tableNumber.innerText = rowCount;
     // Populate all 12 timeslots with cells
     for (let i = 1; i < 13; i++) {
         row.insertCell(i);
         // Create new "timeSlot" property for all cells where we store their respective time slot.
         // Assign start hour into new property (e.g. 9 + 1 = 10:00 )
         row.cells.item(i).timeSlot = 9 + i;
-        addButton(row.cells.item(i), bookingArr, rowCount);
+        loadIndividualCell(row.cells.item(i), bookingArr, rowCount);
     }
 }
 
@@ -86,10 +107,10 @@ async function fetchTables(url) {
 async function doFetch() {
     tableArr = await fetchTables(localTableApi);
     bookingArr = await fetchBookings(localHockeyBookingApi);
+    // Empty table body to prevent duplicates on subsequent generations
+    tableBody.innerHTML = "";
     tableArr.forEach(createRow);
 }
 buttCreateTable.addEventListener('click', doFetch)
 
-//sets date to today's
-//idk how to do time stack overflow doesnt work help!!!!!!!!!!!!!
 document.getElementById('date').valueAsDate = new Date();
