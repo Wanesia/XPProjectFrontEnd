a7887f3556c6e237af2ab8d0638db31055eae8c8
Daniel <dannysht27@gmail.com>
2022-10-05 12:00:26 +0200

    readded dining.css had to delete because of glitch and added delte and put mapping functions in JS currently cancel booking is connected to backend, but backend needs to be updated

--- source/js/dining.js
+++ source/js/dining.js
@@ -17,7 +17,6 @@ console.log(timeInput.value)
 
 dateTimeInput.valueAsDate = new Date();
 
-
 function checkIfFilled()
 {
     if(document.getElementById("firstName").value !== ""
@@ -40,15 +39,17 @@ async function fetchBookedTables()
             booking.diningTable.id = "Table " + booking.diningTable.id
             console.log(booking.diningTable.id)
             tables.forEach(table => {if(table.id === booking.diningTable.id){
-                if(table.style.backgroundColor !== "red")
+                if(table.style.backgroundColor !== "#f00511")
                 {
-                    table.style.backgroundColor="red";
-                    let customer = {
+                    table.style.backgroundColor="#f00511";
+                    const customer = {
                             "firstName": booking.customer.firstName,
                             "lastName": booking.customer.lastName,
                             "phoneNumber": booking.customer.phoneNumber,
                             "tableNumber": booking.diningTable.id,
-                            "timeOfBooking": date[1]
+                            "timeOfBooking": date[1],
+                            "endTimeOfBooking": endDate[1],
+                            "dateOfBooking": dateTimeInput.value
                     }
                     document.getElementById("firstName").value = customer.firstName
                     document.getElementById("lastName").value = customer.lastName
@@ -97,6 +98,56 @@ function padTo2Digits(num) {
     return String(num).padStart(2, '0');
 }
 
+async function restDeleteDiningBooking(booking) {
+    const url = "http://localhost:8080/api/v1/dining-booking";
+
+    const fetchOptions = {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: ""
+    }
+
+    const jsonString = JSON.stringify(booking);
+    fetchOptions.body = jsonString;
+
+    //calls backend and wait for return
+    const response = await fetch(url, fetchOptions).then(fetchBookedTables);
+    response
+    timeOnChange()
+    if (!response.ok) {
+        console.log("Fix bugs");
+    };
+
+    return response;
+}
+
+async function restPutDiningBooking(booking) {
+    const url = "http://localhost:8080/api/v1/dining-booking";
+
+    const fetchOptions = {
+        method: "PUT",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: ""
+    }
+
+    const jsonString = JSON.stringify(booking);
+    fetchOptions.body = jsonString;
+
+    //calls backend and wait for return
+    const response = await fetch(url, fetchOptions).then(fetchBookedTables);
+    response
+    timeOnChange()
+    if (!response.ok) {
+        console.log("Fix bugs");
+    };
+
+    return response;
+}
+
 async function restPostDiningBooking(booking) {
     const url = "http://localhost:8080/api/v1/dining-booking";
 
@@ -112,32 +163,53 @@ async function restPostDiningBooking(booking) {
     fetchOptions.body = jsonString;
 
     //calls backend and wait for return
-    const response = await fetch(url, fetchOptions);
-
-    console.log(response)
+    const response = await fetch(url, fetchOptions).then(fetchBookedTables);
+    response
+    timeOnChange()
     if (!response.ok) {
-        console.log("fuck you");
+        console.log("Fix bugs");
     };
 
     return response;
 }
 
 function addClicker(table) {
-    function func() {
+    function tableClick() {
         inputs.forEach(input=>input.value="")
         createButton.disabled = true;
         startDate.innerHTML = "Date: " + dateTimeInput.value
         time.innerHTML = "Time: " + timeInput.value
         modal.style.display="block";
         modalTitle.innerHTML = table.id;
+        cancelButton.addEventListener("click", e=> {
+            if(table.id == modalTitle.innerHTML && table.style.backgroundColor === "#f00511") {
+                table.style.backgroundColor = "#704F32"
+            }
+            let booking = {
+                startDateTime: dateTimeInput.value + " " + timeInput.value,
+                endDateTime: dateTimeInput.value + " " + padTo2Digits(endDateTime.getHours()) + ":" + padTo2Digits(endDateTime.getMinutes()),
+                customer:{
+                    firstName: document.getElementById("firstName").value,
+                    lastName: document.getElementById("lastName").value,
+                    phoneNumber: document.getElementById("phoneNumber").value,
+                },
+                diningTable:{
+                    id: modalTitle.innerHTML.replace("Table ", ""),
+                    booked:false},
+            }
+            if(booking.customer.firstName != "" && booking.customer.lastName != "" && booking.customer.phoneNumber != "") {
+                restPostDiningBooking(booking)
+            }
+            inputs.forEach(input=>input.value="")
+        })
         createButton.addEventListener("click", e => {modal.style.display="none";
-            if(table.id == modalTitle.innerHTML && table.style.backgroundColor !== "red")
+            if(table.id == modalTitle.innerHTML && table.style.backgroundColor !== "#f00511")
             {
 
-                table.style.backgroundColor="red";
+                table.style.backgroundColor="#f00511";
             }
             let endDateTime = new Date(dateTimeInput.value + " " + timeInput.value)
-            endDateTime.setHours(endDateTime.getHours() + 3);
+            endDateTime.setHours(endDateTime.getHours() + 1);
             let booking = {
                 startDateTime: dateTimeInput.value + " " + timeInput.value,
                 endDateTime: dateTimeInput.value + " " + padTo2Digits(endDateTime.getHours()) + ":" + padTo2Digits(endDateTime.getMinutes()),
@@ -151,17 +223,29 @@ function addClicker(table) {
                     booked:false},
             }
             console.log(booking)
-            if(booking.customer.firstName != "")
+            if(booking.customer.firstName != "" && booking.customer.lastName != "" && booking.customer.phoneNumber != "")
             {
                 restPostDiningBooking(booking)
             }
+            const customer = {
+                "firstName": document.getElementById("firstName").value,
+                "lastName": document.getElementById("lastName").value,
+                "phoneNumber": document.getElementById("phoneNumber").value,
+                "tableNumber": modalTitle.innerHTML.replace("Table ", ""),
+                "timeOfBooking": timeInput.value,
+                "endTimeOfBooking": padTo2Digits(endDateTime.getHours()) + ":" + padTo2Digits(endDateTime.getMinutes()),
+                "dateOfBooking": dateTimeInput.value
+            }
+            console.log(customer)
+            customers.push(customer)
             inputs.forEach(input=>input.value="")
+            //timeOnChange()
         })
         createButton.style.display = "block"
         cancelButton.style.display = "none"
         saveButton.style.display = "none"
         closeButton.addEventListener("click", e=> {modal.style.display="none";inputs.forEach(input=>input.value="")})
-        customers.forEach(customer => {if(table.id == customer.tableNumber)
+        customers.forEach(customer => {if(table.id == customer.tableNumber && timeInput.value >= customer.timeOfBooking && timeInput.value < customer.endTimeOfBooking && dateTimeInput.value === customer.dateOfBooking )
         {
             document.getElementById("firstName").value = customer.firstName
             document.getElementById("lastName").value = customer.lastName
@@ -174,7 +258,7 @@ function addClicker(table) {
     }
     if(timeInput.value <= "21:00")
     {
-        table.addEventListener("click",func)
+        table.addEventListener("click",tableClick)
     }
     else if(timeInput.value > "21:00")
     {
