542b4ed772219c114d2faef142d5343602b58318
Kimgmanis <Kimalexanderkeiser@gmail.com>
2022-10-07 14:53:25 +0200

    injecting HTML notes dynamically

---
+++ source/js/hockey-search.js
@@ -0,0 +1,27 @@
+/* REST API URLs to connect to back-end */
+const localTableApi = "http://localhost:8080/api/v1/hockey-table";
+const localHockeyBookingApi = "http://localhost:8080/api/v1/hockey-booking";
+const hockeySearchButton = document.getElementById("hockeySearchButton");
+const root = document.getElementById("root");
+
+// Arrays to store JSON data from back-end
+let bookingArr = [];
+/* =================================== */
+
+function injectBookings(booking) {
+    const newDiv = document.createElement("div");
+    const newContent = document.createTextNode("Helloo");
+    newDiv.appendChild(newContent);
+    root.appendChild(newDiv);
+}
+
+async function fetchBookings(url) {
+    return fetch(url).then(response => response.json());
+}
+
+async function doFetch() {
+    bookingArr = await fetchBookings(localHockeyBookingApi);
+    bookingArr.forEach(injectBookings);
+}
+
+hockeySearchButton.addEventListener('click',doFetch)
