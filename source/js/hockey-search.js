ed41eb67030d97d7fb37626c941b24c7e1abe55f
bacelarfc <fernandacbacelar@gmail.com>
2022-10-11 22:07:09 +0200

    partially fixed the air hockey search - search by table number works

--- source/js/hockey-search.js
+++ source/js/hockey-search.js
@@ -2,6 +2,7 @@
 const localTableApi = "http://localhost:8080/api/v1/hockey-table";
 const localHockeyBookingApi = "http://localhost:8080/api/v1/hockey-booking";
 const hockeySearchButton = document.getElementById("hockeySearchButton");
+const hockeySearchInput = document.getElementById("search-field");
 const root = document.getElementById("root");
 
 // Arrays to store JSON data from back-end
@@ -9,8 +10,10 @@ let bookingArr = [];
 /* =================================== */
 
 function injectBookings(booking) {
+    console.log(booking)
     const newDiv = document.createElement("div");
     newDiv.classList.add("entity-container");
+    newDiv.innerHTML = booking.customer.firstName;
     root.appendChild(newDiv);
 }
 
@@ -18,9 +21,18 @@ async function fetchBookings(url) {
     return fetch(url).then(response => response.json());
 }
 
+function getSearchInputField(){
+    let inputField = document.getElementById("search-field");
+    console.log(inputField.value);
+    return inputField.value;
+}
+
 async function doFetch() {
-    bookingArr = await fetchBookings(localHockeyBookingApi);
+    console.log(localHockeyBookingApi + "/" + getSearchInputField() )
+    bookingArr = await fetchBookings(localHockeyBookingApi + "/" + getSearchInputField());
+    console.log(bookingArr);
     bookingArr.forEach(injectBookings);
+
 }
 
 hockeySearchButton.addEventListener('click',doFetch)
