bcf09dd3b7fa36a4b1ac4bfc11ea2d9f425a131d
Daniel <dannysht27@gmail.com>
2022-10-11 10:38:08 +0200

    fixing merge conflict with dining-end

--- source/js/dining.js
+++ source/js/dining.js
@@ -8,8 +8,8 @@ const createButton = document.getElementById("createButton")
 const inputs = document.querySelectorAll(".inputs")
 const dateTimeInput = document.querySelector(".date-picker")
 const timeInput = document.querySelector(".time-picker")
-const startDate = document.getElementById("startDate")
-const time = document.getElementById("time")
+const newDate = document.getElementById("newDate")
+const newTime = document.getElementById("newTime")
 const bookedTablesURL = "http://localhost:8080/api/v1/dining-booking"
 const customers=[]
 let bookings
@@ -40,18 +40,17 @@ async function fetchBookedTables()
                 {
                     table.style.backgroundColor="#f00511";
                     const customer = {
-                            "firstName": booking.customer.firstName,
-                            "lastName": booking.customer.lastName,
-                            "phoneNumber": booking.customer.phoneNumber,
-                            "tableNumber": booking.diningTable.id,
-                            "timeOfBooking": date[1],
-                            "endTimeOfBooking": endDate[1],
-                            "dateOfBooking": dateTimeInput.value
+                        "firstName": booking.customer.firstName,
+                        "lastName": booking.customer.lastName,
+                        "phoneNumber": booking.customer.phoneNumber,
+                        "tableNumber": booking.diningTable.id,
+                        "timeOfBooking": date[1],
+                        "endTimeOfBooking": endDate[1],
+                        "dateOfBooking": dateTimeInput.value
                     }
                     document.getElementById("firstName").value = customer.firstName
                     document.getElementById("lastName").value = customer.lastName
                     document.getElementById("phoneNumber").value = customer.phoneNumber
-                    document.getElementById("time").innerHTML = "Time: " + customer.timeOfBooking
                     customers.push(customer)
                     createButton.style.display = "none"
                     cancelButton.style.display = "block"
@@ -109,17 +108,31 @@ async function restDeleteDiningBooking(booking) {
 
     //calls backend and wait for return
     const response = await fetch(url, fetchOptions).then(fetchBookedTables);
-    response
     timeOnChange()
-    if (!response.ok) {
-        console.log("Fix bugs");
-    };
+    return response;
+}
 
+async function restDeleteCustomer(customer) {
+    const url = "http://localhost:8080/api/v1/customer";
+
+    const fetchOptions = {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: ""
+    }
+
+    const jsonString = JSON.stringify(customer);
+    fetchOptions.body = jsonString;
+
+    //calls backend and wait for return
+    const response = await fetch(url, fetchOptions)
     return response;
 }
 
 async function restPutDiningBooking(booking) {
-    const url = "http://localhost:8080/api/v1/dining-booking";
+    const url = "http://localhost:8080/api/v1/dining-booking"
 
     const fetchOptions = {
         method: "PUT",
@@ -133,10 +146,9 @@ async function restPutDiningBooking(booking) {
     fetchOptions.body = jsonString;
 
     //calls backend and wait for return
-    const response = await fetch(url, fetchOptions).then(fetchBookedTables);
-    timeOnChange()
-    console.log(response)
-    return response;
+    const response = await fetch(url, fetchOptions)
+    timeOnChange();
+    return response.status
 }
 
 async function restPostDiningBooking(booking) {
@@ -154,69 +166,78 @@ async function restPostDiningBooking(booking) {
     fetchOptions.body = jsonString;
 
     //calls backend and wait for return
-    const response = await fetch(url, fetchOptions).then(fetchBookedTables);
-    timeOnChange()
-    if (!response.ok) {
-        console.log("Fix bugs");
-    };
-
-    return response;
+    const response = await fetch(url, fetchOptions)//.then(fetchBookedTables)
+    //timeOnChange()
+    return response.status;
 }
 
 function addClicker(table) {
-     function tableClick() {
-        inputs.forEach(input=>input.value="")
+    function tableClick() {
+        inputs.forEach(input=>{input.value=""; input.disabled = null;})
         createButton.disabled = true;
-        startDate.innerHTML = "Date: " + dateTimeInput.value
-        time.innerHTML = "Time: " + timeInput.value
         modal.style.display="block";
+        newTime.value = timeInput.value;
+        newDate.value = dateTimeInput.value;
+        newTime.disabled = true;
+        newDate.disabled = true;
         modalTitle.innerHTML = table.id;
-        saveButton.addEventListener("click", e=>{
-            let endDateTime = new Date(document.getElementById("newDate").value + " " + document.getElementById("newTime").value)
-            endDateTime.setHours(endDateTime.getHours() + 1);
+        document.getElementById("errorText").style.display = "none"
+        saveButton.addEventListener("click", async e => {
             let bookingId;
             bookings.forEach(bookingFetched => {
-                if(bookingFetched.startDateTime == dateTimeInput.value + " " + timeInput.value
-                    && bookingFetched.diningTable.id == modalTitle.innerHTML){
+                let currentEnd = new Date(dateTimeInput.value + " " + timeInput.value)
+                currentEnd.setHours(currentEnd.getHours() + 1);
+                if (bookingFetched.startDateTime <= dateTimeInput.value + " " + timeInput.value
+                    && bookingFetched.endDateTime <= dateTimeInput.value + " " + padTo2Digits(currentEnd.getHours()) + ":" + padTo2Digits(currentEnd.getMinutes())
+                    && bookingFetched.diningTable.id == modalTitle.innerHTML) {
                     bookingId = bookingFetched.id;
-                }})
+                }
+            })
+            let endDateTime = new Date(document.getElementById("newDate").value + " " + document.getElementById("newTime").value)
+            endDateTime.setHours(endDateTime.getHours() + 1);
             let booking = {
                 id: bookingId,
                 startDateTime: document.getElementById("newDate").value + " " + document.getElementById("newTime").value,
                 endDateTime: document.getElementById("newDate").value + " " + padTo2Digits(endDateTime.getHours()) + ":" + padTo2Digits(endDateTime.getMinutes()),
-                customer:{
-                    firstName: document.getElementById("firstName").value,
-                    lastName: document.getElementById("lastName").value,
-                    phoneNumber: document.getElementById("phoneNumber").value,
-                },
-                diningTable:{
+                diningTable: {
                     id: modalTitle.innerHTML.replace("Table ", ""),
-                    booked:false},
+                    booked: false,
+                }
             }
-            if(booking.customer.firstName != "" && booking.customer.lastName != "" && booking.customer.phoneNumber != "") {
-                restPutDiningBooking(booking)
+            if (document.getElementById("firstName").value != "" && document.getElementById("lastName").value != "" && document.getElementById("phoneNumber").value != "") {
+                const responseCode = await restPutDiningBooking(booking)
+                if (responseCode === 400) {
+                    document.getElementById("errorText").innerHTML =  "You are not able to change the booking for the chosen time, please pick a different time."
+                    document.getElementById("errorText").style.display = "block"
+                } else {
+
+                    modal.style.display = "none";
+                }
             }
-            console.log(booking)
-            inputs.forEach(input=>input.value="")
-            modal.style.display = "none";
-            alert()
+
+            inputs.forEach(input => input.value = "")
         })
         cancelButton.addEventListener("click", async e=> {
             if(table.id == modalTitle.innerHTML && table.style.backgroundColor === "#f00511") {
                 table.style.backgroundColor = "#704F32"
             }
-            let endDateTime = new Date(dateTimeInput.value + " " + timeInput.value)
-            endDateTime.setHours(endDateTime.getHours() + 1);
+            let bookingStart
+            let bookingEnd
             let bookingId;
             bookings.forEach(bookingFetched => {
-                if(bookingFetched.startDateTime == dateTimeInput.value + " " + timeInput.value
+                let endDateTime = new Date(bookingFetched.startDateTime)
+                endDateTime.setHours(endDateTime.getHours() + 1);
+                if(bookingFetched.startDateTime <= dateTimeInput.value + " " + timeInput.value
+                    && bookingFetched.endDateTime <= dateTimeInput.value + " " + padTo2Digits(endDateTime.getHours()) + ":" + padTo2Digits(endDateTime.getMinutes())
                     && bookingFetched.diningTable.id == modalTitle.innerHTML){
-                   bookingId = bookingFetched.id;
+                    bookingId = bookingFetched.id;
+                    bookingStart = bookingFetched.startDateTime;
+                    bookingEnd = bookingFetched.endDateTime;
                 }})
             let booking = {
                 id: bookingId,
-                startDateTime: dateTimeInput.value + " " + timeInput.value,
-                endDateTime: dateTimeInput.value + " " + padTo2Digits(endDateTime.getHours()) + ":" + padTo2Digits(endDateTime.getMinutes()),
+                startDateTime: bookingStart,
+                endDateTime: bookingEnd,
                 customer:{
                     firstName: document.getElementById("firstName").value,
                     lastName: document.getElementById("lastName").value,
@@ -234,41 +255,49 @@ function addClicker(table) {
             inputs.forEach(input=>input.value="")
             modal.style.display = "none";
         })
-        createButton.addEventListener("click", e => {modal.style.display="none";
-            if(table.id == modalTitle.innerHTML && table.style.backgroundColor !== "#f00511")
-            {
-
-                table.style.backgroundColor="#f00511";
-            }
+        createButton.addEventListener("click", async e => {
             let endDateTime = new Date(dateTimeInput.value + " " + timeInput.value)
             endDateTime.setHours(endDateTime.getHours() + 1);
             let booking = {
                 startDateTime: dateTimeInput.value + " " + timeInput.value,
                 endDateTime: dateTimeInput.value + " " + padTo2Digits(endDateTime.getHours()) + ":" + padTo2Digits(endDateTime.getMinutes()),
-                customer:{
+                customer: {
                     firstName: document.getElementById("firstName").value,
                     lastName: document.getElementById("lastName").value,
                     phoneNumber: document.getElementById("phoneNumber").value,
                 },
-                diningTable:{
+                diningTable: {
                     id: modalTitle.innerHTML.replace("Table ", ""),
-                    booked:false},
+                    booked: false
+                },
             }
-            if(booking.customer.firstName != "" && booking.customer.lastName != "" && booking.customer.phoneNumber != "")
-            {
-                restPostDiningBooking(booking)
+            if (booking.customer.firstName != "" && booking.customer.lastName != "" && booking.customer.phoneNumber != "") {
+                e.preventDefault()
+                const responseCode = await restPostDiningBooking(booking);
+                if(responseCode === 400)
+                {
+                    document.getElementById("errorText").innerHTML = "You are unable to book this table for this time, as it conflicts with another booking."
+                    document.getElementById("errorText").style.display = "block"
+                    createButton.style.display = "block"
+                    cancelButton.style.display = "none"
+                    saveButton.style.display = "none"
+                }
+                else
+                {
+                    modal.style.display = "none";
+                    const customer = {
+                        "firstName": document.getElementById("firstName").value,
+                        "lastName": document.getElementById("lastName").value,
+                        "phoneNumber": document.getElementById("phoneNumber").value,
+                        "tableNumber": modalTitle.innerHTML.replace("Table ", ""),
+                        "timeOfBooking": timeInput.value,
+                        "endTimeOfBooking": padTo2Digits(endDateTime.getHours()) + ":" + padTo2Digits(endDateTime.getMinutes()),
+                        "dateOfBooking": dateTimeInput.value
+                    }
+                    customers.push(customer)
+                }
             }
-            const customer = {
-                "firstName": document.getElementById("firstName").value,
-                "lastName": document.getElementById("lastName").value,
-                "phoneNumber": document.getElementById("phoneNumber").value,
-                "tableNumber": modalTitle.innerHTML.replace("Table ", ""),
-                "timeOfBooking": timeInput.value,
-                "endTimeOfBooking": padTo2Digits(endDateTime.getHours()) + ":" + padTo2Digits(endDateTime.getMinutes()),
-                "dateOfBooking": dateTimeInput.value
-            }
-            customers.push(customer)
-            inputs.forEach(input=>input.value="")
+            inputs.forEach(input => input.value = "")
         })
         createButton.style.display = "block"
         cancelButton.style.display = "none"
@@ -279,22 +308,38 @@ function addClicker(table) {
             document.getElementById("firstName").value = customer.firstName
             document.getElementById("lastName").value = customer.lastName
             document.getElementById("phoneNumber").value = customer.phoneNumber
-            document.getElementById("time").innerHTML = "Time: " + customer.timeOfBooking
+            document.getElementById("newDate").value = customer.dateOfBooking
+            newTime.value = customer.timeOfBooking
             createButton.style.display = "none"
             cancelButton.style.display = "block"
             saveButton.style.display = "block"
+            inputs.forEach(input => input.disabled = "true")
             document.querySelectorAll(".hidden-input").forEach(input => input.style.display = "block")
-            document.getElementById("startDate").style.display = "none"
-            document.getElementById("time").style.display = "none"
+            newTime.disabled = false;
+            newDate.disabled = false;
         }})
     }
     if(timeInput.value <= "21:00")
     {
         table.addEventListener("click",tableClick)
     }
-    else if(timeInput.value > "21:00")
+    else if(timeInput.value > "21:00" || timeInput.value < "10:00")
     {
         table.replaceWith(table.cloneNode(true))
+    }
+}
+
+function newTimeOnChange()
+{
+    if(newTime.value > "21:00")
+    {
+        newTime.value = "21:00"
+        alert();
+    }
+    if(newTime.value < "10:00")
+    {
+        newTime.value = "10:00"
+        alert();
     }
 }
 
